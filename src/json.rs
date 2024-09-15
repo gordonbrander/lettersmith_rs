b367a546c9447afaf67713f2d4adccc5336ba345
Gordon Brander <gordonbrander@users.noreply.github.com>
2024-09-15 17:57:57 -0400

    Add function to read a JSON file by path

--- src/json.rs
+++ src/json.rs
@@ -1,4 +1,15 @@
 pub use serde_json::Value;
+use std::fs::File;
+use std::io::{BufReader, Result};
+use std::path::Path;
+
+/// Read a JSON file, returning a result of the JSON value.
+pub fn read(path: impl AsRef<Path>) -> Result<Value> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    let value = serde_json::from_reader(reader)?;
+    Ok(value)
+}
 
 /// Merge two JSON values together
 /// https://www.rfc-editor.org/rfc/rfc7396
