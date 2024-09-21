63a4479fa881cdde220d5f21cc5f8f538efa9ed7
Gordon Brander <gordonbrander@users.noreply.github.com>
2024-09-21 11:18:43 -0400

    Add to_tag, index_by_taxonomy, etc

--- src/tags.rs
+++ src/tags.rs
@@ -1,27 +1,79 @@
+use crate::doc::Doc;
 use crate::docs::Docs;
-use crate::json;
+use crate::json::{self, json};
 use crate::stub::Stub;
+use crate::text::remove_non_slug_chars;
 use std::collections::HashMap;
+use tap::Pipe;
 
-/// Given an index-shaped hashmap and a list of keys, return a combined
-/// iterator of the items of those keys.
-pub fn combine_index_keys<'a>(
-    index: &'a HashMap<String, Vec<Stub>>,
-    keys: &'a [String],
-) -> impl Iterator<Item = &'a Stub> + 'a {
-    keys.iter()
-        .flat_map(move |key| index.get(key).into_iter().flatten())
+/// Convert string to tag.
+/// Similar to `to_slug()`, but it replaces spaces with underscores instead
+/// of dashes. This is to make tag slugs compatible with hashtag syntax.
+pub fn to_tag(term: &str) -> String {
+    term.trim()
+        .to_lowercase()
+        .replace(' ', "_")
+        .pipe(|s| remove_non_slug_chars(&s))
+}
+
+/// Given an index-shaped hashmap and a list of keys, return a combined and
+/// deduplicated iterator of the items for those keys.
+pub fn combine_index_values(index: &HashMap<String, Vec<Stub>>, keys: &[String]) -> Vec<Stub> {
+    let mut stubs: Vec<Stub> = keys
+        .iter()
+        .flat_map(move |key| {
+            index
+                .get(key)
+                .into_iter()
+                .flatten()
+                .map(|item| item.to_owned())
+        })
+        .collect();
+    stubs.dedup();
+    stubs
+}
+
+impl Doc {
+    /// Get get tag string values from meta.
+    /// Sluggifies tags to normalize them for string-matching.
+    pub fn get_meta_tags(&self) -> Option<Vec<String>> {
+        match self.meta.get("tags") {
+            Some(json::Value::Array(tag_values)) => {
+                let mut tag_strings: Vec<String> = tag_values
+                    .iter()
+                    .filter_map(|value| value.as_str())
+                    .map(|value| to_tag(value))
+                    .collect();
+                tag_strings.dedup();
+                Some(tag_strings)
+            }
+            _ => None,
+        }
+    }
+
+    /// Set "related" key on meta
+    pub fn set_meta_related(self, related: Vec<Stub>) -> Self {
+        self.merge_meta(json!({ "related": related }))
+    }
+
+    /// Given an index, check the tags in meta, pluck the related
+    /// and set them on the "related" field of meta.
+    pub fn set_meta_related_from_index(self, index: HashMap<String, Vec<Stub>>) -> Self {
+        let Some(tags) = self.get_meta_tags() else {
+            return self;
+        };
+        let related: Vec<Stub> = combine_index_values(&index, &tags);
+        self.merge_meta(json!({
+            "related": related
+        }))
+    }
 }
 
 pub trait TaggedDocs: Docs {
     /// Index docs by taxonomy.
     /// Looks for an array in the meta key specified.
     /// Returns a hashmap of stub lists, indexed by term.
-    /// ```
-    ///    {
-    ///      "term_a": [stub, ...],
-    ///      "term_b": [stub, ...]
-    ///    }
+    /// Terms are sluggified to normalize them for lookup by key.
     fn index_by_taxonomy(&mut self, key: &str) -> HashMap<String, Vec<Stub>> {
         let mut tax_index: HashMap<String, Vec<Stub>> = HashMap::new();
         for doc in self {
@@ -29,7 +81,7 @@ pub trait TaggedDocs: Docs {
                 for term in terms {
                     if let Some(term) = term.as_str() {
                         tax_index
-                            .entry(term.to_string())
+                            .entry(to_tag(term))
                             .or_insert_with(Vec::new)
                             .push(Stub::from(&doc));
                     }
@@ -42,11 +94,6 @@ pub trait TaggedDocs: Docs {
     /// Index docs by tag
     /// Looks for an array in the meta key "tags".
     /// Returns a hashmap of stub lists, indexed by term.
-    /// ```
-    ///    {
-    ///      "term_a": [stub, ...],
-    ///      "term_b": [stub, ...]
-    ///    }
     fn index_by_tag(&mut self) -> HashMap<String, Vec<Stub>> {
         self.index_by_taxonomy("tags")
     }
