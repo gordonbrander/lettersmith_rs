03a05bfbc153d5f623c399ebb2a714eb77ae2caa
Gordon Brander <gordonbrander@users.noreply.github.com>
2024-09-25 17:47:33 -0400

    Add more to prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,4 +1,7 @@
 pub use crate::blog::BlogDocs;
-pub use crate::doc;
+pub use crate::doc::Doc;
 pub use crate::docs::{self, DocResults, Docs};
+pub use crate::error::{Error, ErrorKind};
+pub use crate::json;
+pub use crate::liquid::LiquidDocs;
 pub use crate::markdown::MarkdownDocs;
